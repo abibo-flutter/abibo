075aa101701fe0e3a3dfab61a986ec169bc54d7f
dodo07070707 <aa01056328583@gmail.com>
2023-06-12 19:55:24 +0900

    mainscreenbugfix

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -17,40 +17,38 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          decoration: const BoxDecoration(color: Colors.black),
-          child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/main_screen.png'),
-                fit: BoxFit.fill,
-              ),
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/main_screen.png'),
+              fit: BoxFit.fill,
             ),
-            child: Padding(
-              padding: EdgeInsets.only(
-                top: screenHeight / 844 * 104,
-                bottom: 0,
-                left: screenHeight / 844 * 28,
-                right: screenHeight / 844 * 28,
-              ),
-              child: Column(
-                children: [
-                  const Text(
-                    'ABADFSA',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                    ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: screenHeight / 844 * 104,
+              bottom: 0,
+              left: screenHeight / 844 * 28,
+              right: screenHeight / 844 * 28,
+            ),
+            child: Column(
+              children: [
+                const Text(
+                  'ABADFSA',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
                   ),
-                  const SizedBox(height: 20),
-                  Text(
-                    'Welcome, ${widget.user.email}', // 사용자 이메일 출력 예시
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                    ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Welcome, ${widget.user.email}', // 사용자 이메일 출력 예시
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
