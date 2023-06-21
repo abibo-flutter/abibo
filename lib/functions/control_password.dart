7fda17d50c568b4035de722939a46e5d3c40a434
diw28 <diw28@dimigo.hs.kr>
2023-06-21 11:46:27 +0900

    use json form

--- lib/functions/control_password.dart
+++ lib/functions/control_password.dart
@@ -1,89 +1,67 @@
+import 'dart:convert';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 // 데이터 형식
-/* 
-[
-  'netflix::  {id:qwerty password:qwerty startDate:20230620}  {id:asdfgh password:asdfgh}'
-  'spotify::  {id:qwerty password:qwerty}'
-]
+/*
+{
+  'netflix': [
+    {'id': 'asdfgh', 'password': 'asdfgh'},
+    {'id': 'qwerty', 'password': 'qwerty', 'deadline': 20230621},
+  ],
+  'spotify': [
+    {'id': 'asdfgh', 'password': 'asdfgh'},
+    {'id': 'qwerty', 'password': 'qwerty', 'deadline': 20230621},
+  ],
+}
 */
 void savePassword({
   required String platform,
   required String id,
   required String password,
-  String? startDate,
-  String? endDate,
+  int? deadline,
 }) async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
-  final List<String> serviceList = prefs.getStringList('services') ?? [];
-  String newInfo = '  {id:$id password:$password';
-  if (startDate != null) {
-    newInfo += ' startDate:$startDate';
+  final String serviceJson = prefs.getString('services') ?? '';
+
+  Map<String, List<Map<String, dynamic>>> serviceList =
+      {}; //{platform: [{'...':object}, {'...':object, '...':object}]}
+  serviceList = jsonDecode(serviceJson);
+
+  Map<String, dynamic> newInfo = {
+    'id': id,
+    'password': password,
+  };
+  if (deadline != null) {
+    newInfo['deadline'] = deadline;
   }
-  if (endDate != null) {
-    newInfo += ' endDate:$endDate';
+
+  if (serviceList[platform] != null) {
+    serviceList[platform]!.add(newInfo);
+  } else {
+    serviceList[platform] = [newInfo];
   }
-  newInfo += '}';
-  for (int i = 0; i < serviceList.length; i++) {
-    if (serviceList[i].substring(0, platform.length) == platform) {
-      if (serviceList[i].contains(newInfo) == false) {
-        serviceList[i] += newInfo;
-      }
-      prefs.setStringList('services', serviceList);
-      return;
-    }
-  }
-  prefs.setStringList(
-    'services',
-    [
-      '$platform::$newInfo',
-    ],
-  );
+  prefs.setString('services', jsonEncode(serviceList));
 }
 
-/*
-[
-  {
-    id:'qwerty',
-    password:'qwerty',
-  },
-  {
-    id:'asdfgh',
-    password:'asdfgh',
-  },
-]
-*/
-Future<List<Map<String, String>>?> getPassword({
+Future<List<Map<String, dynamic>>> getPassword({
   String platform = '',
-  String? startDate = '',
-  String? endDate = '',
+  int? startRange,
+  int? endRange,
 }) async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
-  final List<String> serviceList = prefs.getStringList('services') ?? [];
-  List<String>? result;
-  for (int i = 0; i < serviceList.length; i++) {
-    if (serviceList[i].substring(0, platform.length) == platform) {
-      //원하는 플랫폼일 경우
-      result =
-          serviceList[i].split('  ').sublist(1); //{...} 단위로 분류한 후 platform 제거
-      return strToMapList(result);
-    }
-  }
-  return null;
-}
+  final String serviceJson = prefs.getString('services') ?? '';
+  List<Map<String, dynamic>> result = [];
 
-List<Map<String, String>> strToMapList(List<String> str) {
-  Map<String, String> map = {};
-  List<Map<String, String>> result = [];
-  for (var block in str) {
-    //{...} 한개씩 추출
-    block = block.substring(1, block.length - 1); //중괄호 제거
-    for (dynamic property in block.split(' ')) {
-      //'':'' 한개씩 추출
-      property = property.split(':');
-      map[property[0]] = property[1]; //map에 '':'' 추가
+  List<Map<String, dynamic>> serviceList = [];
+  //[{'...':object}, {'...':object, '...':object}]
+  serviceList = jsonDecode(serviceJson)[platform] ?? [];
+
+  for (var service in serviceList) {
+    if ((startRange ?? 0) <= (service['deadline'] ?? 0) &&
+        (service['deadline'] ?? 10e8) <= (endRange ?? 10e8)) {
+      result.add(service);
     }
-    result.add(map); //결과에 map 추가
   }
-  return result; //[{'':'', '':'',}, {'':'',},] 형태로 반환
+  return result;
 }
