261d828d93037c51a79166b2d5f454add40cee82
diw28 <diw28@dimigo.hs.kr>
2023-06-22 09:18:54 +0900

    change data form

--- lib/functions/control_password.dart
+++ lib/functions/control_password.dart
@@ -4,16 +4,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 // 데이터 형식
 /*
-{
-  'netflix': [
-    {'id': 'asdfgh', 'password': 'asdfgh'},
-    {'id': 'qwerty', 'password': 'qwerty', 'Subscribe': 20230621},
-  ],
-  'spotify': [
-    {'id': 'asdfgh', 'password': 'asdfgh'},
-    {'id': 'qwerty', 'password': 'qwerty', 'Subscribe': 20230621},
-  ],
-}
+
+platform-netflix: [
+  {'id': 'asdfgh', 'password': 'asdfgh'},
+  {'id': 'qwerty', 'password': 'qwerty', 'Subscribe': 20230621},
+],
+platform-spotify: [
+  {'id': 'asdfgh', 'password': 'asdfgh'},
+  {'id': 'qwerty', 'password': 'qwerty', 'Subscribe': 20230621},
+],
+
 */
 void savePassword({
   required String platform,
@@ -24,10 +24,10 @@ void savePassword({
   int? cost,
 }) async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
-  final String serviceJson = prefs.getString('services') ?? '';
+  final String serviceJson = prefs.getString('platform-$platform') ?? '[]';
 
-  Map<String, List<Map<String, dynamic>>> serviceList =
-      {}; //{platform: [{'...':object}, {'...':object, '...':object}]}
+  List<Map<String, dynamic>> serviceList = [];
+  //platform-name: [{'...':object}, {'...':object, '...':object}]
   serviceList = jsonDecode(serviceJson);
 
   Map<String, dynamic> newInfo = {
@@ -44,12 +44,9 @@ void savePassword({
     newInfo['cost'] = cost;
   }
 
-  if (serviceList[platform] != null) {
-    serviceList[platform]!.add(newInfo);
-  } else {
-    serviceList[platform] = [newInfo];
-  }
-  prefs.setString('services', jsonEncode(serviceList));
+  serviceList.add(newInfo);
+
+  prefs.setString('platform-$platform', jsonEncode(serviceList));
 }
 
 Future<List<Map<String, dynamic>>> getPassword({
@@ -62,12 +59,12 @@ Future<List<Map<String, dynamic>>> getPassword({
   int? costMax,
 }) async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
-  final String serviceJson = prefs.getString('services') ?? '';
+  final String serviceJson = prefs.getString('platform-$platform') ?? '[]';
   List<Map<String, dynamic>> result = [];
 
   List<Map<String, dynamic>> serviceList = [];
   //[{'...':object}, {'...':object, '...':object}]
-  serviceList = jsonDecode(serviceJson)[platform] ?? [];
+  serviceList = jsonDecode(serviceJson);
 
   for (var service in serviceList) {
     if ((payDateMin ?? 0) > (service['payDate'] ?? 0) ||
