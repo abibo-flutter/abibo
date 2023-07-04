e0afd2473682ab13b2b250ba107a8316902d3f75
dodo07070707 <aa01056328583@gmail.com>
2023-07-04 12:00:59 +0900

    updating

--- lib/screens/platforms.dart
+++ lib/screens/platforms.dart
@@ -3,14 +3,14 @@ import 'package:abibo/screens/change_pin_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:abibo/screens/theme/text_theme.dart';
+import 'package:abibo/widgets/mainmenucard.dart';
 import 'package:get/get.dart';
-import 'package:abibo/screens/platforms.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class Platforms extends StatefulWidget {
+  const Platforms({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<Platforms> createState() => _PlatformsState();
 }
 
 void navigateToInitPINScreen() {
@@ -21,7 +21,7 @@ void navigateToChangePINScreen() {
   Get.to(() => const ChangePINScreen());
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _PlatformsState extends State<Platforms> {
   late SharedPreferences prefs;
   String? PIN;
 
@@ -85,30 +85,22 @@ class _HomeScreenState extends State<HomeScreen> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        SizedBox(
-                          width: 50,
-                          height: 50,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Get.to(
-                                  const Platforms()); // 다음페이지에는 이동하려는 페이지 위젯을 넣어주세요
-                            },
-                            child: const Text('플랫폼'),
-                          ),
+                        const MainMenu(
+                          color: Color(0xFFC87CFF),
+                          image: 'assets/images/mainmenu1.png',
+                          headline: '플랫폼',
+                          text: '여기다가 아무거나\n적을거고 이거는\n진짜 아무거나이니까\n나중에 바꾸자',
+                          topLeft: false,
                         ),
                         SizedBox(
                           width: screenWidth / 390 * 6,
                         ),
-                        SizedBox(
-                          width: 50,
-                          height: 50,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Get.to(
-                                  const Platforms()); // 다음페이지에는 이동하려는 페이지 위젯을 넣어주세요
-                            },
-                            child: const Text('OTT'),
-                          ),
+                        const MainMenu(
+                          color: Color(0xFF6153D1),
+                          image: 'assets/images/mainmenu2.png',
+                          headline: 'OTT',
+                          text:
+                              '사용하고 있는 \nOTT 서비스의 아이디와 가입일을\n 입력하고 보다 쉽게 해지일을\n확인하세요!',
                         ),
                       ],
                     ),
@@ -118,30 +110,21 @@ class _HomeScreenState extends State<HomeScreen> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        SizedBox(
-                          width: 50,
-                          height: 50,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Get.to(
-                                  const Platforms()); // 다음페이지에는 이동하려는 페이지 위젯을 넣어주세요
-                            },
-                            child: const Text('보증서'),
-                          ),
+                        const MainMenu(
+                          color: Color(0xFF0A033C),
+                          image: 'assets/images/mainmenu3.png',
+                          headline: '보증서',
+                          text: '사실 이만큼 안써도 되고\n그냥 적당히 한두줄만\n써도 됨!',
                         ),
                         SizedBox(
                           width: screenWidth / 390 * 6,
                         ),
-                        SizedBox(
-                          width: 50,
-                          height: 50,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Get.to(
-                                  const Platforms()); // 다음페이지에는 이동하려는 페이지 위젯을 넣어주세요
-                            },
-                            child: const Text('메모'),
-                          ),
+                        const MainMenu(
+                          color: Color(0xFF7443D3),
+                          image: 'assets/images/mainmenu4.png',
+                          headline: '메모',
+                          text: '여기다가는 그냥 대충\n기능 간단하게 소개할 \n수 있는 문구 넣으면 \n좋을듯',
+                          bottomRight: false,
                         ),
                       ],
                     )
--- lib/widgets/mainmenucard.dart
+++ lib/widgets/mainmenucard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:abibo/screens/theme/text_theme.dart';
+import 'package:get/get.dart';
+import 'package:abibo/screens/platforms.dart';
 
 class MainMenu extends StatelessWidget {
   final bool topRight;
@@ -26,71 +28,77 @@ class MainMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
-    return Container(
-      width: screenWidth / 390 * 168,
-      height: screenHeight / 844 * 230,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.only(
-          topRight: (topRight) ? const Radius.circular(60) : Radius.zero,
-          topLeft: (topLeft) ? const Radius.circular(60) : Radius.zero,
-          bottomLeft: (bottomLeft) ? const Radius.circular(60) : Radius.zero,
-          bottomRight: (bottomRight) ? const Radius.circular(60) : Radius.zero,
+    return GestureDetector(
+      onTap: () {
+        Get.to(() => const Platforms());
+      },
+      child: Container(
+        width: screenWidth / 390 * 168,
+        height: screenHeight / 844 * 230,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.only(
+            topRight: (topRight) ? const Radius.circular(60) : Radius.zero,
+            topLeft: (topLeft) ? const Radius.circular(60) : Radius.zero,
+            bottomLeft: (bottomLeft) ? const Radius.circular(60) : Radius.zero,
+            bottomRight:
+                (bottomRight) ? const Radius.circular(60) : Radius.zero,
+          ),
+          image: DecorationImage(
+            image: AssetImage(image),
+            fit: BoxFit.cover,
+          ),
         ),
-        image: DecorationImage(
-          image: AssetImage(image),
-          fit: BoxFit.cover,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            SizedBox(
+              height: screenHeight / 844 * 12,
+            ),
+            Container(
+              width: screenWidth / 390 * 16,
+              height: 1,
+              decoration: const BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: screenHeight / 844 * 2,
+            ),
+            Container(
+              width: screenWidth / 390 * 8,
+              height: 1,
+              decoration: const BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: screenHeight / 844 * 26,
+            ),
+            Text(
+              headline,
+              style: ABTextTheme.MainWidgetTitle,
+            ),
+            SizedBox(
+              height: screenHeight / 844 * 16,
+            ),
+            Container(
+              width: screenWidth / 396 * 22,
+              height: 1,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: screenHeight / 844 * 12,
+            ),
+            Text(
+              text,
+              style: ABTextTheme.MainWidgetContext,
+              textAlign: TextAlign.center,
+            )
+          ],
         ),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          SizedBox(
-            height: screenHeight / 844 * 12,
-          ),
-          Container(
-            width: screenWidth / 390 * 16,
-            height: 1,
-            decoration: const BoxDecoration(
-              color: Colors.black,
-            ),
-          ),
-          SizedBox(
-            height: screenHeight / 844 * 2,
-          ),
-          Container(
-            width: screenWidth / 390 * 8,
-            height: 1,
-            decoration: const BoxDecoration(
-              color: Colors.black,
-            ),
-          ),
-          SizedBox(
-            height: screenHeight / 844 * 26,
-          ),
-          Text(
-            headline,
-            style: ABTextTheme.MainWidgetTitle,
-          ),
-          SizedBox(
-            height: screenHeight / 844 * 16,
-          ),
-          Container(
-            width: screenWidth / 396 * 22,
-            height: 1,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-            ),
-          ),
-          SizedBox(
-            height: screenHeight / 844 * 12,
-          ),
-          Text(
-            text,
-            style: ABTextTheme.MainWidgetContext,
-            textAlign: TextAlign.center,
-          )
-        ],
       ),
     );
   }
