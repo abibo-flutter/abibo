f789f0ea083d3fb45d1b32e499eea996ce4275bc
diw28 <diw28@dimigo.hs.kr>
2023-07-06 21:37:26 +0900

    resetresetresetresetresetresetresetresetresetreset

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'screens/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:get/get.dart';
+import 'screens/pin_screen.dart';
 import 'screens/theme/color_theme.dart';
-import 'screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +13,12 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  Future<String?> getPIN() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? PIN = prefs.getString('PIN');
+    return PIN;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +32,20 @@ class MyApp extends StatelessWidget {
           primary: AbColor.ABMainColor,
         ),
       ),
-      home: const SplashScreen(),
+      home: FutureBuilder(
+        future: getPIN(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (snapshot.hasData) {
+              return const PINScreen();
+            } else {
+              return const MainScreen();
+            }
+          }
+        },
+      ),
     );
   }
 }
