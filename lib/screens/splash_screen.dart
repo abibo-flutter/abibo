16e47d9c7bc292343936b791eae571f364aaf553
diw28 <diw28@dimigo.hs.kr>
2023-07-06 18:38:52 +0900

    af

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,41 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:abibo/screens/after_splash_screen.dart';
-import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
 
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  void navigateToNextPage() async {
+    // 로딩이 완료된 후 NextPage로 이동
+    await Future.delayed(const Duration(seconds: 3), () {
+      Get.offAll(() => const AfterSplashScreen());
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     navigateToNextPage();
   }
 
-  void navigateToNextPage() {
-    // 로딩이 완료된 후 NextPage로 이동
-    Future.delayed(const Duration(seconds: 3), () {
-      Get.offAll(() => const AfterSplashScreen());
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Lottie.asset(
-          'assets/animations/wave_animation.json',
-          width: 400,
-          height: 400,
-          fit: BoxFit.contain,
-        ),
-      ),
+          // child: Lottie.asset(
+          //   'assets/animations/wave_animation.json',
+          //   width: 400,
+          //   height: 400,
+          //   fit: BoxFit.contain,
+          // ),
+          ),
     );
   }
 }
