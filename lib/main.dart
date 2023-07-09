2c70925a5a7b669ad4958f3f55403e9d4c8088e9
dodo07070707 <aa01056328583@gmail.com>
2023-07-09 20:16:35 +0900

    splashscreen create

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,39 @@
-import 'screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/pin_screen.dart';
 import 'screens/theme/color_theme.dart';
+import 'screens/main_screen.dart';
+import 'screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _showSplashScreen = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    await Future.delayed(
+        const Duration(seconds: 3)); // splash screen이 표시될 시간(초)
+    setState(() {
+      _showSplashScreen = false;
+    });
+  }
 
   Future<String?> getPIN() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -31,20 +53,22 @@ class MyApp extends StatelessWidget {
           primary: AbColor.ABMainColor,
         ),
       ),
-      home: FutureBuilder(
-        future: getPIN(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            if (snapshot.hasData) {
-              return const PINScreen();
-            } else {
-              return const MainScreen();
-            }
-          }
-        },
-      ),
+      home: _showSplashScreen
+          ? const Scaffold(body: SplashScreen())
+          : FutureBuilder(
+              future: getPIN(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const SplashScreen();
+                } else {
+                  if (snapshot.hasData) {
+                    return const PINScreen();
+                  } else {
+                    return const MainScreen();
+                  }
+                }
+              },
+            ),
     );
   }
 }
