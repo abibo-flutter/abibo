107ad20cf9eeaebf4af54beecc6e1da5f8c3f802
diw28 <diw28@dimigo.hs.kr>
2023-07-11 20:33:39 +0900

    sort register info screen

--- lib/screens/register_info_screen.dart
+++ lib/screens/register_info_screen.dart
@@ -215,6 +215,40 @@ class _RegisterInfoScreenState extends State<RegisterInfoScreen> {
                         color: Colors.white,
                       ),
                     ),
+                  if (isGuarantee)
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  if (isGuarantee)
+                    TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          brand = value.removeAllWhitespace;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: "브랜드를 입력하세요",
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.6),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white.withOpacity(0.6),
+                            width: 1.5,
+                          ),
+                        ),
+                        focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 1.5,
+                          ),
+                        ),
+                      ),
+                      style: const TextStyle(
+                        // 입력중 text color
+                        color: Colors.white,
+                      ),
+                    ),
                   if (isSubscription || isGuarantee)
                     const SizedBox(
                       height: 10,
@@ -262,40 +296,6 @@ class _RegisterInfoScreenState extends State<RegisterInfoScreen> {
                       },
                       decoration: InputDecoration(
                         hintText: "금액을 입력하세요",
-                        hintStyle: TextStyle(
-                          color: Colors.white.withOpacity(0.6),
-                        ),
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.white.withOpacity(0.6),
-                            width: 1.5,
-                          ),
-                        ),
-                        focusedBorder: const UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.white,
-                            width: 1.5,
-                          ),
-                        ),
-                      ),
-                      style: const TextStyle(
-                        // 입력중 text color
-                        color: Colors.white,
-                      ),
-                    ),
-                  if (isGuarantee)
-                    const SizedBox(
-                      height: 10,
-                    ),
-                  if (isGuarantee)
-                    TextField(
-                      onChanged: (value) {
-                        setState(() {
-                          brand = value.removeAllWhitespace;
-                        });
-                      },
-                      decoration: InputDecoration(
-                        hintText: "브랜드를 입력하세요",
                         hintStyle: TextStyle(
                           color: Colors.white.withOpacity(0.6),
                         ),
