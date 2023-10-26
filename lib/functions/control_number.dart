34b99ce24573df8718c4a6392ebe3eb2ef37436a
dodo07070707 <aa01056328583@gmail.com>
2023-10-26 20:33:52 +0900

    commit reset

---
+++ lib/functions/control_number.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+class CounterController extends GetxController {
+  var objectCount = 0.obs;
+
+  void objectCountincrease() {
+    objectCount.value++;
+    if (objectCount.value == 2) {
+      Get.defaultDialog(
+        title: "더 많은 정보 등록하기",
+        middleText: "아비보+를 결재해 더 많은 정보를 등록해보세요",
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Get.back(); // 다이얼로그 닫기
+            },
+            child: const Text("확인"),
+          ),
+        ],
+        navigatorKey: Get.nestedKey(0),
+      );
+    }
+  }
+
+  void objectCountdecrease() {
+    objectCount.value--;
+  }
+}
